9f702423641d82a790f8cefc9b54f143ff564bea
Angel-Journey <angel.journey7@gmail.com>
2021-08-17 22:22:55 -0500

    Add steps & footer containers
    Add four step boxes and begin test button in steps container
    Add text to footer containers
    Add spacing changes to all containers on homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import authService from "../../services/authService";
 import { useForm } from '../../hooks/useForm'
 
-export default function Signup (props) {
+export default function Homepage (props) {
   const history = useHistory();
   const formRef = useRef();
   const [message, updateMessage] = useState('')
@@ -14,154 +14,125 @@ export default function Signup (props) {
     passwordConf: "",
   });
 
-  useEffect(() => {
-    formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
-    updateMessage('');
-  }, [formValue]);
+  // useEffect(() => {
+  //   formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+  //   updateMessage('');
+  // }, [formValue]);
 
-  const handleSubmit = async (e) => {
-    const { handleSignupOrLogin } = props;
-    console.log(formValue.email, formValue.password)
-    e.preventDefault();
-    try {
-      if(formValue.password !== formValue.passwordConf)
-        throw Error("Passwords must match")
-      await authService.signup(formValue);
-      handleSignupOrLogin()
-      history.push("/");
-    } catch (err) {
-      updateMessage(err.message);
-    }
-  };
+  // const handleSubmit = async (e) => {
+  //   const { handleSignupOrLogin } = props;
+  //   console.log(formValue.email, formValue.password)
+  //   e.preventDefault();
+  //   try {
+  //     if(formValue.password !== formValue.passwordConf)
+  //       throw Error("Passwords must match")
+  //     await authService.signup(formValue);
+  //     handleSignupOrLogin()
+  //     history.push("/");
+  //   } catch (err) {
+  //     updateMessage(err.message);
+  //   }
+  // };
 
     return (
       // header container
       <div>
         <div className="flex mb-4 justify-center">
           <div className="flex justify-center">
-            <img width="350"
+            <img width="500"
             src="/images/team-spirt.png"
             alt='team-spirit'
             />
           </div>
-          <div className="mt-6">
-          <h2 className="mt-6 text-3xl text-black font-bold flex items-center justify-left">Diverse world.</h2>
-          <h2 className="mt-6 text-5xl text-black font-extrabold flex items-center justify-left">Diverse team.</h2>
+          <div className="mt-14">
+          <h2 className="mt-6 text-4xl text-black font-bold flex items-center justify-left">Diverse world.</h2>
+          <h2 className="mt-6 text-6xl text-black font-extrabold flex items-center justify-left">Diverse team.</h2>
           </div>
         </div>
 
       {/* search section */}
       <div className="bg-homepage">
         <div className="pt-6">
-        <h2 className="pl-10 text-2xl text-white flex items-center justify-left">Work culture found easily...</h2>
-        <h2 className="pl-10 text-1xl text-white flex items-center justify-left">Search for a company's D&I score here</h2>
+        <h2 className="pl-20 text-4xl text-white flex items-center justify-left">Work culture found easily...</h2>
+        <h2 className="pl-20 text-1xl text-white flex items-center justify-left">Search for a company's D&I score here</h2>
         </div>
-        <div className="p-10 pt-2 pb-8">
-          <div className="bg-white flex items-center justify-center rounded-full shadow-xl">
+        <div className="pl-20 pt-2 pb-8">
+          <div className="bg-white w-10/12 flex items-center justify-center rounded-full shadow-xl">
             <input className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="🔍   company name">
             </input>
             <div className="p-4">
-              <button className="bg-search text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none flex items-center justify-center">
+              <button className="bg-search text-white rounded-full p-2 w-40 hover:bg-altbox focus:outline-none flex items-center justify-center font-bold">
                 Search
               </button>
             </div>
           </div>
         </div>
+      </div>
+
+      {/* steps section */}
+      <div className="ml-4 mr-4 bg-white">
+        <div className="pt-6">
+        <h2 className="pb-6 text-4xl text-black flex items-center justify-center">Get your company score in 4 steps</h2>
         </div>
 
-      <div className="min-h-screen bg-white flex">
-      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm lg:w-96">
-          <div className="flex justify-center">
-          <img width="150"
-          src="/images/Auto-Mastery-fav.png"
-          alt='logo'
-          />
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-4 gap-4">
+
+            <div className="bg-firstbox border-none font-bold leading-4 box-border h-130 w-275 p-4 rounded-lg">
+              <div className="font-extrabold text-3xl">
+              1
+              </div>
+              <div className="ml-6">
+              Answer the questions in the D&I Test and fill out the data needed for a comprehensive score
+              </div>
+            </div>
+            <div className="bg-altbox border-none font-bold leading-4 box-border h-130 w-275 p-4 rounded-lg">
+              <div className="font-extrabold text-3xl">
+              2
+              </div>
+              <div className="ml-6">
+              Set up your company account and verify the claim on the company
+              </div>
+            </div>
+            <div className="bg-homepage border-none font-bold leading-4 box-border h-130 w-275 p-4 rounded-lg">
+              <div className="font-extrabold text-3xl">
+              3
+              </div>
+              <div className="ml-6">
+              Set up your company account and verify the claim on the company
+              </div>
+            </div>
+            <div className="bg-altbox border-none font-bold leading-4 box-border h-130 w-275 p-4 rounded-lg">
+              <div className="font-extrabold text-3xl">
+              4
+              </div>
+              <div className="ml-6">
+              Receive your score via e-mail and listed on your profile
+              </div>
+            </div>
+
           </div>
-          <h2 className="mt-6 text-3xl font-extrabold text-gray-900">Sign Up for an account</h2>
-          <div className="mt-8">
+        </div>
 
-            <div className="mt-6">
-              <form ref={formRef} autoComplete='off' onSubmit={handleSubmit}className="space-y-6">
-              {message && <p>{message}</p>}
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                    Email address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="on"
-                      onChange={handleChange}
-                      value={formValue.email}
-                      required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
+        <div className="p-10 pt-6 pb-8">
+            <div className="p-4 flex items-center justify-center">
+              <button className="bg-test text-white rounded-full w-5/12 p-2 hover:bg-homepage focus:outline-none flex items-center justify-center font-extrabold">
+                Begin Test
+              </button>
+            </div>
+        </div>
+      </div>
 
-                <div className="space-y-1">
-                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="off"
-                      onChange={handleChange}
-                      value={formValue.password}
-                      required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-
-                <div className="space-y-1">
-                  <label htmlFor="passwordConf" className="block text-sm font-medium text-gray-700">
-                    Confirm Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="confim"
-                      name="passwordConf"
-                      type="password"
-                      autoComplete="off"
-                      onChange={handleChange}
-                      value={formValue.passwordConf}
-                      required
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <button
-                    disabled={formInvalid}
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Sign up
-                  </button>
-                </div>
-              </form>
-              &nbsp;&nbsp;
-              <Link to='/login'>
-              <h3>Already have an account? <p className='text-red-500 font-bold' href='/login'>Log in</p></h3>
-              </Link>
+        <div className="bg-navbar text-white">
+          <div className="flex items-center justify-center">
+            <div className="grid grid-cols-4 gap-4 m-20">
+              <div>How it works</div>
+              <div>Resources</div>
+              <div>Contact US</div>
+              <div>Company D&I Test</div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="hidden lg:block relative w-0 flex-1">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="/images/car-unsplash.jpg"
-          alt=""
-        />
-      </div>
-    </div>
       </div>
     );
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,11 @@ module.exports = {
     backgroundColor: theme => ({
        ...theme('colors'),
        'homepage': '#588AA6', // background color for homepage
-       'search': '#E29C32', // search button color
-       'danger': '#e3342f',
+       'search': '#E29C32', // search button color for homepage
+       'test': '#2A3B50', // test button color for homepage
+       'firstbox': '#93C9E7', // first box color for steps container on homepage
+       'altbox': '#9DDBCC', // alt box color for steps container on homepage
+       'navbar': '#111A25', // alt box color for steps container on homepage
       })
   },
   variants: {
