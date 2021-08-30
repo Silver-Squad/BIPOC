249903b6e4e61eeb5add48002f8bfb5fed7d8568
cmosley <cmosley@beholdmedia.com>
2021-08-29 20:59:14 -0500

    tab content changes

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,8 +4,8 @@ import { UsersIcon } from '@heroicons/react/solid'
 const SearchBar = props => {
 
   return (
-    <div className="mt-4 w-5/6 mx-auto flex">
-          <form>
+    <form>
+      <div className="mt-4 w-5/6 mx-auto flex">
           <div className="relative flex items-stretch flex-grow focus-within:z-10">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
               <UsersIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -19,14 +19,15 @@ const SearchBar = props => {
               placeholder="Company Name"
             />
           </div>
+          
           <button
             type="button"
             className="inline-flex items-center px-20 py-2 border border-transparent text-md font-medium rounded-full shadow-sm text-white bg-yellow-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Search
-          </button>
-          </form>
-        </div>
+          </button>        
+      </div>
+    </form>
   )
 }
 
